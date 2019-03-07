fa2aac6ee06ecafc2b2543ddac7fc01ad5edca80
Mikaela Sheldt <msheldt@gmail.com>
2019-03-07 16:41:25 -0500

    Initial draft of SERVER BOILERMAKER

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const express = require('express')
 const volleyball = require('volleyball')
 const morgan = require('morgan');
 
+const db = require('./db.js')
+
 const app = express()
 
 // logging middleware
@@ -18,7 +20,7 @@ app.use(express.urlencoded({ extended: true }))
 // static middleware
 //app.use(express.static(path.join(__dirname, '../public')))
 
-app.use('/api', require('./api')) // include our routes!
+app.use('/api', require('./api'))
 
 app.get('*', (req, res) => {
   res.send("Hello Ya'll")
@@ -30,9 +32,17 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).send(err.message || 'Internal server error')
 })
 
-const port = process.env.PORT || 3000; // this can be very useful if you deploy to Heroku!
-app.listen(port, function () {
-  console.log(`I am practicing active listening on port ${port}`);
-});
+const port = process.env.PORT || 3000;
+ // this can be very useful if you deploy to Heroku!
+
+db.sync()
+  .then(() => {
+    console.log('db synced')
+    app.listen(port, function () {
+      console.log(`I am practicing active listening on port ${port}`)
+    })
+  })
+
+// if you update your db schemas, make sure you drop the tables first and then recreate them
 
 module.exports = app
